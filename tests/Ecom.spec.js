f2613810f049ac1e126db1546bbc60fafdb3f65d
shilka <shilka0209@gmail.com>
2026-04-17 15:04:13 +0530

    8 th commit

--- tests/Ecom.spec.js
+++ tests/Ecom.spec.js
@@ -7,13 +7,15 @@ test('Ecom login', async ({ page }) => {
 await page.goto("https://acn-malabar-stage.malabargoldanddiamonds.com/in/kl/en/jewellery.html");
 
 
-
+ const email = 'shilkacp@gmail.com';
 const icon = page.locator('img[src$="Personperson.svg"]');
 await expect(icon).toBeVisible();
 await icon.click();
 
 const signIn = page.getByText('Sign In / Register');
 await signIn.click();
+
+
     
 
 });
